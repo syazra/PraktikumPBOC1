38d83924cfeed27e7967491fa0755fad5cc5313d
Syafira Azka Ramadhani <syafirazka15@gmail.com>
2026-02-19 17:17:08 +0700

    test commit keempat

--- IntroTest/src/MTitik.java
+++ IntroTest/src/MTitik.java
@@ -1,3 +1,8 @@
+/* Nama File  : MTitik.java */
+/* Deskripsi  : berisi atribut dan method dalam class Titik */
+/* Pembuat    : 24060124130088 - Syafira Azka Ramadhani */
+/* Tanggal    : 19/02/2026 - 15.40 */
+
 public class MTitik {
     public static void main(String[] args) {
         Titik T1 = new Titik();
