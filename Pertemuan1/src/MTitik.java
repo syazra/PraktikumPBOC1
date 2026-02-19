c107f557bcfa39c91ab9f1297fea4bb9eb791827
Syafira Azka Ramadhani <syafirazka15@gmail.com>
2026-02-19 20:30:12 +0700

    prak_1

---
+++ Pertemuan1/src/MTitik.java
@@ -0,0 +1,15 @@
+/* Nama File  : MTitik.java */
+/* Deskripsi  : berisi main program class Titik */
+/* Pembuat    : 24060124130088 - Syafira Azka Ramadhani */
+/* Tanggal    : 19/02/2026 - 15.40 */
+
+public class MTitik {
+    public static void main(String[] args) {
+        Titik T1 = new Titik();
+        T1.setAbsis(3);
+        T1.setOrdinat(4);
+        T1.printTitik();
+        T1.geser(3, 4);
+        T1.printTitik();
+    }
+}
