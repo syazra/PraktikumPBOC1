6ae719588e87412e687ef50880d93a00ec4ed73e
Syafira Azka Ramadhani <syafirazka15@gmail.com>
2026-03-11 21:39:24 +0700

    update pert1

--- Pertemuan1/src/MTitik.java
+++ Pertemuan1/src/MTitik.java
@@ -5,11 +5,20 @@
 
 public class MTitik {
     public static void main(String[] args) {
-        Titik T1 = new Titik();
-        T1.setAbsis(3);
-        T1.setOrdinat(4);
-        T1.printTitik();
-        T1.geser(3, 4);
-        T1.printTitik();
+        Titik T1 = new Titik(); // membuat objek titik T1 (0, 0)
+        Titik T2 = new Titik(); // membuat objek titik T2 (0, 0)
+
+        T1.setAbsis(3); // mengubah absis T1 dengan nilai 3
+        T1.setOrdinat(4); // mengubah ordinat T1 dengan nilai 4
+        T1.printTitik(); // mencetak koordinat T1 ke layar
+        T1.geser(3, 4); // menggeser T1 sejauh (3, 4)
+        T1.printTitik(); // mencetak koordinat T1 setelah digeser
+
+        T2.setAbsis(9); // mengubah absis T2 dengan nilai 9
+        T2.setOrdinat(-1); // mengubah ordinat T2 dengan nilai -1
+        T2.printTitik(); // mencetak koordinat T2 ke layar
+        T2.geser(-11, 6); // menggeser T2 sejauh (-11, 6)
+        System.out.println("Absis T2: " + T2.getAbsis()); // mencetak absis T2 setelah digeser
+        System.out.println("Ordinat T2: " + T2.getOrdinat()); // mencetak absis T2 setelah digeser
     }
 }
